1cd0be1cc66ba73f70a3f8a621e58919e3bf7323
Rojalin Barik <rojalinbarik2005@gmail.com>
2025-03-14 20:32:00 +0530

    add cookie-parser and cors middleware to enhance request handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
-app.use(express.json({limit:"20kb"}))
+const app = express();
+
+app.use(express.json({ limit: "20kb" }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"))
+app.use(cookieParser())
+
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import connectDB from "./database/index.js";
 import dotenv from "dotenv";
-import app from "./app.js"
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 connectDB()
-.then(()=>{
-    app.on("error",(error)=>{
-        console.log("Server Error :: ",error);
-        throw error
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Server Error :: ", error);
+      throw error;
     });
-    app.listen(process.env.PORT || 1000 ,()=>{
-      console.log(`Server is Running at ${process.env.PORT}`)
+    app.listen(process.env.PORT || 1000, () => {
+      console.log(`Server is Running at ${process.env.PORT}`);
     });
-})
-.catch((error)=>{
-    console.log("MONGODB CONNECTION ERROR ::",error)
-})
+  })
+  .catch((error) => {
+    console.log("MONGODB CONNECTION ERROR ::", error);
+  });

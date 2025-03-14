3ab112e5df1ff9b7213b9965e0f0d68fcb81c439
Rojalin Barik <rojalinbarik2005@gmail.com>
2025-03-14 21:29:26 +0530

    add error and response handler utilities for improved error management

---
+++ src/utils/ResponseHandler.js
@@ -0,0 +1,11 @@
+class ResponseHandler{
+    constructor(statusCode,data,message="Success",pagonation=null){
+        this.statusCode = statusCode
+        this.data = data
+        this.message=message
+        if(pagonation){
+            this.pagonation = pagonation
+        }
+    }
+}
+export {ResponseHandler}

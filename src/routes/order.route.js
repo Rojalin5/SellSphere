5296fcdc9132ae6839dcffa59b79e6f32229c0c9
Rojalin Barik <rojalinbarik2005@gmail.com>
2025-04-14 00:26:43 +0530

    feat: enhance order routes with additional functionalities for fetching, updating, and canceling orders

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/Authentication.js";
-import { createOrder } from "../controllers/order.controller.js";
+import { authorizedRole } from "../middlewares/roleAuthentication.js";
 
-const  router = Router()
+import {
+  createOrder,
+  getUserOrders,
+  getOrderById,
+  getAllOrders,
+  orderStatusUpdate,
+  cancelOrder,
+} from "../controllers/order.controller.js";
 
-router.route("/product-order").post(verifyJWT,createOrder)
+const router = Router();
 
-export default router
+router.route("/product-order").post(verifyJWT, createOrder);
+router.route("/get-orders").get(verifyJWT,getUserOrders);
+router.route("/order-ByID/:id").get(verifyJWT,getOrderById)
+router.route("/all-orders").get(verifyJWT,authorizedRole("Admin"),getAllOrders)
+router.route("update-status").patch(verifyJWT,authorizedRole("Admin"),orderStatusUpdate)
+router.route("/cancel-order").patch(verifyJWT,cancelOrder)
+export default router;
